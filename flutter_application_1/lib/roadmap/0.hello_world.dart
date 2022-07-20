839e819fc0e52a1eeb384c3510ef7df9da5d3c4a
ricky-blip <ricky.rinaldy77@gmail.com>
2022-07-20 08:02:22 +0700

    update

---
+++ flutter_application_1/lib/roadmap/0.hello_world.dart
@@ -0,0 +1,29 @@
+/** SECTION
+ * 1. import package flutter material dart
+ * 2. runApp dan panggil namaClass di main
+ * 3. type 'stl' lalu pilih flutter stateless/statefull widget
+ * 4. samakan namaClass nya dengan yg ada di main 
+ * 5. dibagian Widget build(), return widget MaterialApp(), lalu home:
+ * 6. di dalam materialapp, ada widget scaffold(), lalu body:
+ * 7. di dalam scaffold(), akan ada banyak widget2 yg bisa digunakan.
+ *    - ada widget Text() untuk membuat tulisan.
+ *    - ada widget 'Colors.' dan 'color()' untuk memberi warna.
+ * 8. untuk center gunakan Center()
+ */
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        body: Center(child: Text("Hello World")),
+      ),
+    );
+  }
+}
