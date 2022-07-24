46174a25edbf4fc3a5faf2f16ff6383aad504d05
ricky-blip <ricky.rinaldy77@gmail.com>
2022-07-25 05:56:20 +0700

    update

---
+++ flutter_application_1/lib/roadmap/3.visible_invisible_widget.dart
@@ -0,0 +1,11 @@
+/** SECTION invisible widget / layouting widget
+ * di intro basic widget itu kita sebut visible/kelihatan widgetnya
+ * invisible widget seperti:
+ * 1. 
+ */
+
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
