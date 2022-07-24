99454162926cd31c0afae4ec754866546af04444
ricky-blip <ricky.rinaldy77@gmail.com>
2022-07-24 12:38:23 +0700

    update

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,2 +1,22 @@
 /** SECTION 
  */
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key); // ignore this
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        // backgroundColor: Colors.indigo,
+        body: Center(child: Text("Hello World!")),
+      ),
+    );
+  }
+}
