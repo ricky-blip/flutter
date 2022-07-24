ae3134aff7b8566e7b4b55ab859bd3e17ee1fbc1
ricky-blip <ricky.rinaldy77@gmail.com>
2022-07-24 10:06:31 +0700

    update

--- flutter_application_1/lib/roadmap/0.hello_world.dart
+++ flutter_application_1/lib/roadmap/0.hello_world.dart
@@ -1,19 +1,18 @@
-/** SECTION
- * 1. import package flutter material dart
- * 2. runApp dan panggil namaClass di main
- * 3. type 'stl' lalu pilih flutter stateless/statefull widget
- * 4. samakan namaClass nya dengan yg ada di main 
- * 5. dibagian Widget build(), return widget MaterialApp(), lalu home:
- * 6. di dalam materialapp, ada widget scaffold(), lalu body:
- * 7. di dalam scaffold(), akan ada banyak widget2 yg bisa digunakan.
- *    - ada widget Text() untuk membuat tulisan.
- *    - ada widget 'Colors.' dan 'color()' untuk memberi warna.
- * 8. untuk center gunakan Center()
- * 
- * Ada yg dinamakan Name Argument:
- * - home:
- * - body:
- */
+/// SECTION
+/// 1. import package flutter material dart
+/// 2. runApp dan panggil namaClass di main
+/// 3. type 'stl' lalu pilih flutter stateless/statefull widget
+/// 4. samakan namaClass nya dengan yg ada di main 
+/// 5. dibagian Widget build(), return widget MaterialApp(), lalu home:
+/// 6. di dalam materialapp, ada widget scaffold(), lalu body:
+/// 7. di dalam scaffold(), akan ada banyak widget2 yg bisa digunakan.
+///    - ada widget Text() untuk membuat tulisan.
+///    - ada widget 'Colors.' dan 'color()' untuk memberi warna.
+/// 8. untuk center gunakan Center()
+/// 
+/// Ada yg dinamakan Name Argument:
+/// - home:
+/// - body:
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,6 +20,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key); // ignore this
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
