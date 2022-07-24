fc39669738434be40462f0a7a6b5608c35446811
ricky-blip <ricky.rinaldy77@gmail.com>
2022-07-24 14:19:01 +0700

    update

--- flutter_application_1/lib/roadmap/1.basic_widget.dart
+++ flutter_application_1/lib/roadmap/1.basic_widget.dart
@@ -1,8 +1,8 @@
 /// SECTION Intro Basic Widget
 /// Ada yg dinamakan Name Argument:
-/// - appbar: , title: , child: , image: 
-/// 
-/// NOTE 1.widget AppBar di appbar: di dalam Scaffold untuk judul 
+/// - appbar: , title: , child: , image:
+///
+/// NOTE 1.widget AppBar di appbar: di dalam Scaffold untuk judul
 /// NOTE 2.widget Container di body: di dalam Scaffold untuk membuat kotak/container
 /// NOTE 3.widget FlutterLogo di body: di dalam Scaffold untuk membuat logo default flutter
 /// NOTE 4.widget ElevateButton di body: utk membuat tombol dgn onPressed == sebuah fungsi, child == sebuah tombol
@@ -12,9 +12,9 @@
 ///        2. Network Image -> gambar yg di import dari internet menggunakan url link
 ///        3. File Image (rarely used)
 ///        4. Memory Image (rarely used)
-/// NOTE untuk mengimport image diperlukan setting di 'pubspec.yaml' 
+/// NOTE untuk mengimport image diperlukan setting di 'pubspec.yaml'
 ///      di bagian to add asset lalu ubah ke path image-nya
-/// 
+///
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,7 +29,7 @@ class BasicWidget extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("This is App Bar"),
+          title: Text("'App Bar' Basic Widget"),
         ),
         body: Center(
           child: Image(
