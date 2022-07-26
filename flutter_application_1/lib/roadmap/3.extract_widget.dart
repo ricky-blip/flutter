63390c74fd0b3d88a4e213e610d96bedb47ef260
ricky-blip <ricky.rinaldy77@gmail.com>
2022-07-26 11:58:31 +0700

    update

---
+++ flutter_application_1/lib/roadmap/3.extract_widget.dart
@@ -0,0 +1,37 @@
+/// jika kita tidak ingin membuat banyak container berulang-ulang
+/// gunakan extract widget di satu container saja
+/// lalu buat namaClass nya, dan akan dibuatkan otomatis classnya
+/// karna container hanya berbeda di text dan warna nya saja
+/// kita atur text dan warna:
+import 'package:flutter/material.dart';
+import '/widgets/box_color.dart';
+
+void main() {
+  runApp(const ExtractWidget());
+}
+
+class ExtractWidget extends StatelessWidget {
+  const ExtractWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Extract Widget"),
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: const [
+              BoxColor(text: "RED", warna: Colors.red),
+              BoxColor(text: "YELLOW", warna: Colors.yellow),
+              BoxColor(text: "GREEN", warna: Colors.green),
+              BoxColor(text: "BLUE", warna: Colors.blue),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
