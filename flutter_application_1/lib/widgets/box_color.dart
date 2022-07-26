168bdb43e93003221a04dc379c0cde5ce052e55f
ricky-blip <ricky.rinaldy77@gmail.com>
2022-07-26 14:16:20 +0700

    update

--- flutter_application_1/lib/widgets/box_color.dart
+++ flutter_application_1/lib/widgets/box_color.dart
@@ -5,6 +5,7 @@ class BoxColor extends StatelessWidget {
   final String text;
   final Color warna;
   // constructor with name parameter {}
+  // bisa dikasih nullable jika tidak required(wajib aja)
   // ignore: use_key_in_widget_constructors
   const BoxColor({
     required this.text,
