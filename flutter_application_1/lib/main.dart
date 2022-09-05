b23574fad7e6ec87a8307d5bcc71baab9076e06b
ricky-blip <ricky.rinaldy77@gmail.com>
2022-09-05 20:27:08 +0700

    api indonesia region with packages dropdown search

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -62,7 +62,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: IndonesiaRegionDropDownAPI(),
     );
--- flutter_application_1/lib/roadmap/4_packages_flutter/dropdown_API/indonesian_region.dart
+++ flutter_application_1/lib/roadmap/4_packages_flutter/dropdown_API/indonesian_region.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:http/http.dart' as myhttp;
 
 class IndonesiaRegionDropDownAPI extends StatefulWidget {
   const IndonesiaRegionDropDownAPI({Key? key}) : super(key: key);
@@ -15,6 +17,12 @@ class _IndonesiaRegionDropDownAPIState
     return Scaffold(
       appBar: AppBar(
         title: Text("API Indonesia Region"),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          DropdownSearch(),
+        ],
       ),
     );
   }
