62447184baa6b6769b90a9324085df8ff2f581e9
ricky-blip <ricky.rinaldy77@gmail.com>
2022-09-06 21:24:03 +0700

    Update

--- flutter_application_1/lib/roadmap/4_packages_flutter/dropdown_API/indonesian_region.dart
+++ flutter_application_1/lib/roadmap/4_packages_flutter/dropdown_API/indonesian_region.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:dropdown_search/dropdown_search.dart';
 import 'package:http/http.dart' as myhttp;
@@ -12,6 +14,8 @@ class IndonesiaRegionDropDownAPI extends StatefulWidget {
 
 class _IndonesiaRegionDropDownAPIState
     extends State<IndonesiaRegionDropDownAPI> {
+  final String apiKey =
+      "9b5be0e757f9690bbbb750ea67afadaf9426e7a6fe52ba4568ed24f46e018752";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +25,36 @@ class _IndonesiaRegionDropDownAPIState
       body: ListView(
         padding: EdgeInsets.all(20),
         children: [
-          DropdownSearch(),
+          DropdownSearch<String>(
+            popupProps: PopupProps.dialog(
+              showSelectedItems: true,
+              showSearchBox: true,
+            ),
+            onChanged: (value) => print(value),
+            //NOTE function use for getting data
+            asyncItems: (text) async {
+              //NOTE get from url
+              var response = await myhttp.get(
+                Uri.parse(
+                    "https://api.binderbyte.com/wilayah/provinsi?api_key=$apiKey"),
+              );
+              //NOTE logic if cant get/not connect from URL
+              if (response.statusCode != 200) {
+                return [];
+              }
+              //NOTE decode data body and only get "value" to Mapping and declare as list allProvice
+              List allProvice = (json.decode(response.body) as Map)["value"];
+              //NOTE get data name from value and declare as list allNameProvice
+              List<String> allNameProvince = [];
+              //NOTE Looping data name from value for get data nameProvince
+              allProvice.forEach((element) {
+                allNameProvince.add(
+                  element["name"],
+                );
+              });
+              return allNameProvince;
+            },
+          ),
         ],
       ),
     );
